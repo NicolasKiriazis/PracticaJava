962dd7f1841392f3638ad1d5a8ff0df1041e45ca
Nicolas Kiriazis <nicolaskiriazis@gmail.com>
2024-02-23 16:13:01 -0300

    Clase 1 practica con tipos de datos

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,36 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        /* EJERCICIO PARA CALCULAR PRETAMO MAS INTERES, LA IDEA ES APRENDER TIPOS DE DATOS Y VARIABLE*/
+
+        short capitalSolicitado = 12500;
+        float interes = 1.75f;
+        byte periodoEnMeses = 24;
+
+        float resultado = (capitalSolicitado * interes) * periodoEnMeses;
+
+        System.out.println(resultado);
+
+        /*EJERCICIO PARA CALCULAR LA PARTE ENTERA DE UN DATO FLOAT*/
+
+        /*float numero = 2.50f;
+        int entero = (int)numero;
+
+        System.out.println("El entero vale " + entero);*/
+
+        /*EJERCICIO PARA OBTENER LA PARTE DECIMAL DE UN DATO FLOAT*/
+
+        float numero = 2.50f;
+        int entero = (int)numero;
+        float decimal = (numero - entero);
+        System.out.println("El decimal vale " + decimal);
+
+
+
+
+
+
+
     }
 }
